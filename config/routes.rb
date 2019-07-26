29de12af6ca145970694ddde12d97b6a248d60cf
Sergio Burgueño <burguer@gmail.com>
2019-07-25 19:45:26 -0700

    add jwt and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :posts
+
+  devise_for :users,
+             path: '',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             }
+  resources :users
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

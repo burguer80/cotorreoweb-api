83e30e368dd20cc8b6b5ba5bd63c41d3c5cd5f66
Sergio Burgueño <burguer@gmail.com>
2019-07-26 14:03:44 -0700

    Add cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from CanCan::AccessDenied, with: :role_access_denied
+
 
   def render_resource(resource)
     if resource.errors.empty?
@@ -9,16 +13,28 @@ class ApplicationController < ActionController::API
     end
   end
 
+  private
+
   def validation_error(resource)
+    error_response(:bad_request, resource.errors)
+  end
+
+  def role_access_denied
+    error_response(:unauthorized, 'Role access denied')
+  end
+
+  def record_not_found
+    error_response(:not_found, 'Resource not found')
+  end
+
+  def error_response(status, details)
     render json: {
-        errors: [
-            {
-                status: '400',
-                title: 'Bad Request',
-                detail: resource.errors,
-                code: '100'
-            }
-        ]
-    }, status: :bad_request
+      errors: [
+        {
+          status: status,
+          detail: details
+        }
+      ]
+    }, status: status
   end
 end

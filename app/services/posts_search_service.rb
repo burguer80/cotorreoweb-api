b8d925d20ecbe3fc424969ae0f3520ccb6d9405d
Sergio Burgueño <burguer@gmail.com>
2020-02-26 19:15:09 -0800

    Add UNACCENT method into the SQL

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,7 +1,7 @@
 class PostsSearchService
   def self.search(curr_posts, query)
     posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in:1.hours) do
-      curr_posts.where("title like '%#{query}%'").ids
+      curr_posts.where('UNACCENT(title) ILIKE UNACCENT(?)', "%#{query}%").ids
     end
 
     curr_posts.where(id: posts_ids)

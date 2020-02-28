4e54c43c503c6977c24c742aeb76e635f4127083
Sergio Burgueño <burguer@gmail.com>
2020-02-27 21:45:27 -0800

    fix serializer issue

--- app/serializers/post_serializer.rb
+++
@@ -1,7 +0,0 @@
-class PostSerializer < ActiveModel::Serializer
-  attributes :id, :body, :created_at, :status, :title, :user
-
-  def user
-    instance_options[:without_serializer] ? object.user : UserSerializer.new(object.user, without_serializer: true)
-  end
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,3 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_name, :email
-end
